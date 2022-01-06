21ca027e826b722aef198f5936d458abe571edb2
marti <martina01.natali@edu.unife.it>
2022-01-06 16:22:34 +0100

    Added function to read files to read_claro.cpp, added secondolotto_1 trial folder. Next step is looping over all files of a single directory

--- read_claro.cpp
+++ read_claro.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <fstream>
+// #include <cstdlib>
 #include <string>
+
+void readFile(const std::string& FILE)
+{
+    std::ifstream inf (FILE.c_str());
+
+    if (!inf)
+    {
+        std::cerr << "Error: Unable to open file" << std::endl;
+        exit(1);
+    }
+
+    while (inf)
+    {
+        std::string line;
+        std::getline(inf, line);
+        std::cout << line << std::endl;
+    }
+}
 
 int main()
 // This may seem stupid but it is COMPULSORY to call the main function main(). Not doing it leads to the error "undefined reference to `WinMain'".
@@ -8,5 +28,6 @@ int main()
     std::string FILEPATH {}; //"../secondolotto_1/Station_1__11/2017_10_31_09_46_11_Tray_Station_1__11/Chip_001/S_curve"};                       //?
     std::cin >> FILEPATH;
     std::cout << "You entered: " << "\"" + FILEPATH + "\"\n";
+    readFile(FILEPATH);
     return 0;
 }
