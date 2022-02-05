6b102ac12cc01b7aed080b65d3bee094ba2e8920
marti <martina01.natali@edu.unife.it>
2022-02-05 11:42:32 +0100

    Concluded branch_claro. Starting working on branch_sipm.

--- pyproj/branch_claro/Def/.ipynb_checkpoints/README-checkpoint.md
+++ pyproj/branch_claro/Def/.ipynb_checkpoints/README-checkpoint.md
@@ -2,6 +2,6 @@
 
 All kinds of errors raised are correctly handled: well done! 
 
-- [ ] create file/log where to store paths of non-fittable data
-- [ ] tell user how many data couldn't be fitted (count above paths)
-- [ ] implemet possibility of plotting data even if fit parameters are bad
+- [x] create file/log where to store paths of non-fittable data
+- [x] tell user how many data couldn't be fitted (count above paths)
+- [x] implemet possibility of plotting data even if fit parameters are bad
--- pyproj/branch_claro/Def/.ipynb_checkpoints/claro_fit-checkpoint.py
+++ pyproj/branch_claro/Def/.ipynb_checkpoints/claro_fit-checkpoint.py
@@ -7,6 +7,7 @@ import re
 import csv
 import time
 import fnmatch
+import warnings
 import numpy as np
 import pandas as pd
 import matplotlib as mplt
@@ -19,6 +20,8 @@ from scipy.optimize import curve_fit
 #                FILEPATH="Ch_*_offset_*_Chip_*.txt")
 # 
 # def read_data(path)
+#
+# def fileinfo_find(fileinfo, chip, ch, station='1', sub='11',)
 # 
 # def fit_erf(x, y, META, npoints=1000)
 # 
@@ -39,7 +42,8 @@ from scipy.optimize import curve_fit
 #################################################################################################
 
 def fileinfo(TDIR, DIRPATH="*Station_1__*\Station_1__??_Summary\Chip_???\S_curve",
-               FILEPATH="Ch_*_offset_*_Chip_*.txt"):
+             FILEPATH="Ch_*_offset_*_Chip_*.txt",
+             custom_n_files='all',):
     """Function that walks on all subdirectories of TDIR that match DIRPATH and FILEPATH.
     
     Returns
@@ -79,6 +83,8 @@ def fileinfo(TDIR, DIRPATH="*Station_1__*\Station_1__??_Summary\Chip_???\S_curve
     for root, dirs, files in os.walk(TDIR):
         if fnmatch.fnmatch(root, TDIR + DIRPATH):
             for f in files:
+                if custom_n_files!='all':
+                    if tot_counts>custom_n_files: break
                 if fnmatch.fnmatch(f, FILEPATH):
                     tot_counts += 1
                     thisfile = os.path.join(root,f)
@@ -116,10 +122,10 @@ def fileinfo(TDIR, DIRPATH="*Station_1__*\Station_1__??_Summary\Chip_???\S_curve
                             pass
     
     bad_counts = tot_counts-good_counts
-    print("\nProcess completed in %s s." % (format(time.time()-start_time,".2f")))
+    print("\n\nProcess completed in %s s." % (format(time.time()-start_time,".2f")))
     print(f"\nTotal number of files found: {tot_counts}.")
     print(f"Total number of good files: {good_counts}. Output paths to good files are stored in {OUTFILE}")
-    print(f"Total number of bad files: {bad_counts}. Output paths to bad files are stored in {OUTBAD}")
+    print("Total number of bad files: {0[0]} ({0[1]:.1%}) out of total). Output paths to bad files are stored in {0[2]}".format([bad_counts, bad_counts/tot_counts, OUTBAD]))
     
     return fileinfos
 
@@ -144,6 +150,7 @@ def read_data(path):
     TRANSITION = data.iloc[0,1]
     WIDTH = -data.iloc[0,2]
     META = {
+        'path' : path,
         'amplitude' : AMPLITUDE,
         'transition' : TRANSITION,
         'width' : WIDTH
@@ -153,7 +160,8 @@ def read_data(path):
 #################################################################################################
 
 def fit_erf(x, y, meta, guesses='default',
-            interactive=True, npoints=1000):
+            interactive=True, log=True, warnings_ignore=True, 
+            npoints=1000):
     """Fit the provided x,y data with a modified erf function.
 
     If interactive==True prints message to tell the user if the fitting procedure converged.
@@ -170,16 +178,23 @@ def fit_erf(x, y, meta, guesses='default',
     
     # Sample dict of fit parameters
     fit_dict = {
-            'params' : list(np.zeros(3)), #[amplitude, transition, width/2]
-            'errors' : list(np.zeros(3)),
+            #'params' : list(np.zeros(3)), #[amplitude, transition, width/2]
+            #'errors' : list(np.zeros(3)),
             'code' : 'bad'
         }
     
     try:
         if guesses=='default':
             guesses=[meta['amplitude'], meta['transition'], meta['amplitude']/2]
-            
+        
+        bad_list = []            
         fit_params, fit_covar = curve_fit(func, x, y, p0=guesses)
+        
+        with warnings.catch_warnings(record=True) as w:
+            if w:
+                if log==True: bad_list.append(meta['path'])
+            if warnings_ignore==True: warnings.simplefilter("ignore")
+
         fit_dev = np.sqrt(np.diag(fit_covar))
         all_params = np.append(fit_params, fit_dev)
         
@@ -187,13 +202,17 @@ def fit_erf(x, y, meta, guesses='default',
             if i == np.inf:
                 raise ValueError(f'Fit returned an inf parameter. Please try with different GUESSES.')
             elif i < 1e-6:
-                raise ValueError(f'Fit returned a parameter that is < 1e-6 or negative. Please try with different GUESSES.')
+                raise ValueError(f'Fit returned a parameter that is < 1e-6 or negative. Please try with different set of guesses.')
         if interactive==True: print("Fit ok!")
                 
     except ValueError as err:
+        if log==True:
+            if log==True: bad_list.append(meta['path'])
         if interactive==True: print(f'Error: {err}.\nThe fit parameters are not returned.')
             
     except RuntimeError as err:
+        if log==True:
+            if log==True: bad_list.append(meta['path'])
         if interactive==True: print(err)
     
     else: 
@@ -205,6 +224,8 @@ def fit_erf(x, y, meta, guesses='default',
         return fit_dict
     
     finally:
+        with open("log_unfit.txt", "a") as output:
+            if bad_list: output.write(bad_list[0]+"\n")
         return fit_dict
 
 #################################################################################################
@@ -221,35 +242,26 @@ def func(x, ampl, a, b):
     
 #################################################################################################
 
-
 def plot_fit(x, y, metafit, fileinfo, npoints=1000,
-             interactive=False,
+             interactive=False, log=True,
              show_scatter=True, show_fit=True, show_transition=True,
              save=False, save_path='.\plot', save_format='pdf', show=True,
              **kwargs):
-    """
-    [Function documentation]
+    """[Function documentation]
+    
     """
     
-
     title = f"Fit CLARO: station {fileinfo['station']}, chip {fileinfo['chip']}, ch {fileinfo['ch']}"
-
+    
     font = {
-        'fontsize': plt.rcParams['axes.titlesize'],
-        'fontweight' : plt.rcParams['axes.titleweight'],
-        'verticalalignment': 'baseline',
-        # 'horizontalalignment': plt.loc,
+    'fontsize': plt.rcParams['axes.titlesize'],
+    'fontweight' : plt.rcParams['axes.titleweight'],
+    'verticalalignment': 'baseline',
+    # 'horizontalalignment': plt.loc,
     }
     
     plt.figure(figsize=(5,5), tight_layout=True)
     plt.title(title, fontdict=font)
-
-    # x,y data computed as a grid of npoints and using parameters of the fit
-    fit_params = metafit['params']
-    fit_errs = metafit['errors']
-    
-    xfit = np.linspace(x.min(), x.max(), npoints)
-    yfit= func(xfit, *fit_params)
     
     # Show scatterplot of data (default = True)
     if show_scatter == True:
@@ -261,11 +273,22 @@ def plot_fit(x, y, metafit, fileinfo, npoints=1000,
         plt.xlabel('x [arb units]', fontdict=font)
         plt.ylabel('y [arb units]', fontdict=font)
         # plt.legend(loc='lower right', fontsize = 8)
-        
+    
     try:
         if metafit['code']=='bad':
+            if log==True:
+                with open("log_unplot.txt", "a") as output:
+                    output.write(fileinfo['path']+"\n")
             raise ValueError("Error: the provided fit has invalid parameters. "
-                             +f"Plot {title} will not be plotted.\r")
+                             +f"Plot {title} will not be plotted. Unfitted files' paths in log_unplot.txt\r")
+    
+        # x,y data computed as a grid of npoints and using parameters of the fit
+        fit_params = metafit['params']
+        fit_errs = metafit['errors']
+        
+        xfit = np.linspace(x.min(), x.max(), npoints)
+        yfit= func(xfit, *fit_params)
+        
                              
         # Show plot of fit (default = True)
         if show_fit == True: 
@@ -298,5 +321,6 @@ def plot_fit(x, y, metafit, fileinfo, npoints=1000,
     
     if show == True:
         plt.show()
+        plt.close()
     else:
         plt.close()
